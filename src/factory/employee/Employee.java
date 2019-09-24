76552c20a2c6d576d8fbb265cf3be60def6320af
Marcin <marcin96semkowski@gmail.com>
2019-09-24 18:56:25 +0200

    equals() and hashCode() Employee

--- src/factory/employee/Employee.java
+++ src/factory/employee/Employee.java
@@ -5,13 +5,13 @@ import java.util.Set;
 
 public class Employee {
 
-private String name;
+    private String name;
 
-private int age;
+    private int age;
 
-private String department;
+    private String department;
 
-
+    private int experience;
 
 
     public Employee(String name, int age, String work) {
@@ -19,6 +19,14 @@ private String department;
         this.age = age;
         this.department = work;
 
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
     }
 
     public String getName() {
@@ -43,5 +51,42 @@ private String department;
 
     public void setDepartment(String department) {
         this.department = department;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        if (obj instanceof Employee) {
+            Employee employeeObject = (Employee) obj;
+            if (this.name.equals(employeeObject.name) &&
+                    (this.age == employeeObject.age) &&
+                    (this.department.equals(employeeObject.department)) &&
+                    (this.experience == this.experience)) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 7;
+        result = 31 * result + (name == null ? 0 : name.hashCode());
+        result = 31 * result + age;
+        result = 31 * result + (department == null ? 0 : department.hashCode());
+        result = 31 * result + experience;
+        return result;
     }
 }
