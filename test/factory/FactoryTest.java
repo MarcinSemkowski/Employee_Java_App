582b227faf8a1ee2c17e8b8c38d51799e54c3a1d
Marcin <marcin96semkowski@gmail.com>
2019-09-28 16:55:21 +0200

    Add method update department test

--- test/factory/FactoryTest.java
+++ test/factory/FactoryTest.java
@@ -11,47 +11,48 @@ import static org.junit.Assert.*;
 
 public class FactoryTest {
 
-    private  String nameDepTest;
+    private String nameDepTest;
 
     Factory factory;
     Department departmentTest;
 
     @Before
-    public void init(){
+    public void init() {
         factory = new Factory("dev");
-         nameDepTest = "developers";
-         departmentTest = new Department(nameDepTest);
+        nameDepTest = "developers";
+        departmentTest = new Department(nameDepTest);
+        departmentTest.setNightShift(true);
     }
 
 
     @Test
     public void addToMapDepartment() {
-      //given
+        //given
         departmentTest.setNightShift(false);
         //when
-        boolean isDepAdded = factory.addToMapDepartment(departmentTest.getName(),departmentTest.isNightShift());
+        boolean isDepAdded = factory.addToMapDepartment(departmentTest.getName(), departmentTest.isNightShift());
         //then
-          assertTrue(isDepAdded);
+        assertTrue(isDepAdded);
     }
 
     @Test
     public void deleteFromMapDepartment() {
         //given
-       departmentTest.setNightShift(true);
-        factory.addToMapDepartment(departmentTest.getName(),departmentTest.isNightShift());
+        departmentTest.setNightShift(true);
+        factory.addToMapDepartment(departmentTest.getName(), departmentTest.isNightShift());
         String[] arrayKeyTest = new String[factory.getDepartmentMap().size()];
-        int i =0;
+        int i = 0;
         //when
-        for (String keyTest: factory.getDepartmentMap().keySet()) {
-           arrayKeyTest[i++] = keyTest;
+        for (String keyTest : factory.getDepartmentMap().keySet()) {
+            arrayKeyTest[i++] = keyTest;
         }
-          //then
-        assertFalse(factory.deleteFromMapDepartment(0,arrayKeyTest));
+        //then
+        assertFalse(factory.deleteFromMapDepartment(0, arrayKeyTest));
     }
 
     @Test
-    public void showDepartmentsFromMapDepartment(){
-       //give
+    public void showDepartmentsFromMapDepartment() {
+        //give
         departmentTest.setNightShift(false);
         StringBuilder keysTest = new StringBuilder();
         keysTest.append("-----------------------");
@@ -64,9 +65,27 @@ public class FactoryTest {
         keysTest.append("\n");
         keysTest.append("-----------------------");
         //when
-        factory.getDepartmentMap().put(departmentTest.getName(),departmentTest);
+        factory.getDepartmentMap().put(departmentTest.getName(), departmentTest);
         //then
-        assertEquals(keysTest.toString(),factory.showDepartmentsFromMapDepartment());
+        assertEquals(keysTest.toString(), factory.showDepartmentsFromMapDepartment());
+
+    }
+
+    @Test
+    public void updateToMapDepartment() {
+        //given
+        int indexTest = 0;
+        factory.addToMapDepartment("developers", false);
+        String[] keysTest = new String[factory.getDepartmentMap().size()];
+        String updateDepTest = " dep";
+        int i = 0;
+        //when
+        for (String keyTest : factory.getDepartmentMap().keySet()) {
+            keysTest[i++] = keyTest;
+        }
+        boolean isUpdated = factory.updateToMapDepartment(indexTest, keysTest, updateDepTest, true);
+        //then
+        assertTrue(isUpdated);
 
     }
 }
