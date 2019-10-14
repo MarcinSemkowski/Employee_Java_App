3a0e98e5312137ccf620ac8bf91ad00791c458da
Marcin <marcin96semkowski@gmail.com>
2019-10-14 13:34:42 +0200

    Add Sort employees by age #1 test

--- test/factory/DepartmentTest.java
+++ test/factory/DepartmentTest.java
@@ -13,11 +13,19 @@ import static org.junit.Assert.*;
 public class DepartmentTest {
 
     Department department;
+    Employee employee;
+    Employee employee1;
+    Employee employee2;
+    Employee employee3;
 
     @Before
     public void init() {
         department = new Department("Drivers");
         department.setNightShift(true);
+         employee = new Employee("Christopher", 25, department, 5);
+         employee1 = new Employee("David", 46, department, 7);
+         employee2 = new Employee("Alexander", 50, department, 8);
+         employee3 = new Employee("Brandon", 30, department, 3);
 
     }
 
@@ -32,25 +40,24 @@ public class DepartmentTest {
         //then
         assertThat(department.showEmployeeMethods(), is(employeeMethodsTest));
     }
+
     @Test
-    public void showSortMethodsTest(){
-       //given
+    public void showSortMethodsTest() {
+        //given
         List<String> sortMethodsTest = Arrays.asList(
                 "1.By sortować alfabetycznie  \n"
                 , " By sortować po wieku:\n"
-                ,"2. Od najstarszego do najmłodszego \n"
-                ,"3. Od najmłodszego do najstarszego \n "
+                , "2. Od najstarszego do najmłodszego \n"
+                , "3. Od najmłodszego do najstarszego \n "
         );
         //then
-        assertThat(department.showSortMethods(),is(sortMethodsTest));
+        assertThat(department.showSortMethods(), is(sortMethodsTest));
     }
+
     @Test
-    public void sortAlphabeticallyTest(){
+    public void sortAlphabeticallyTest() {
         //given
-        Employee employee = new Employee("Christopher",25,department,4);
-        Employee employee1 = new Employee("David",30,department,8);
-        Employee employee2 = new Employee("Alexander",30,department,8);
-        Employee employee3 = new Employee("Brandon",30,department,8);
+
         Set<Employee> employeesTest = new HashSet<>();
         //when
         employeesTest.add(employee);
@@ -65,7 +72,28 @@ public class DepartmentTest {
         department.addEmployeeToDepartment(employee3);
         department.setEmployees(department.sortAlphabetically());
         //then
-        assertThat(employeesTest,is(not(department.getEmployees())));
+        assertThat(employeesTest, is(not(department.getEmployees())));
+    }
+
+    @Test
+    public void sortEmployeesByAgeReversed() {
+        //given
+        Set<Employee> employeeSetTest = new LinkedHashSet<>();
+        //when
+        employeeSetTest.add(employee);
+        employeeSetTest.add(employee3);
+        employeeSetTest.add(employee1);
+        employeeSetTest.add(employee2);
+
+        department.addEmployeeToDepartment(employee);
+        department.addEmployeeToDepartment(employee1);
+        department.addEmployeeToDepartment(employee2);
+        department.addEmployeeToDepartment(employee3);
+        department.setEmployees(department.sortByAgeReversed());
+        //then
+        assertThat(department.getEmployees(),is(employeeSetTest));
+
+
     }
 
 
@@ -90,28 +118,30 @@ public class DepartmentTest {
         //when
         boolean isAdd = department.addEmployeeToDepartment(new Employee(name, age, department, experience));
         boolean isDeleted = department.deleteEmployee(new Employee(name, age, department, experience));
-         //then
+        //then
         assertTrue(isAdd);
         assertTrue(isDeleted);
     }
 
     @Test
-    public void updateEmployeeTest(){
+    public void updateEmployeeTest() {
         //given
         String name = "Mike";
         int age = 25;
         int experience = 3;
-        Employee oldEmployee = new Employee(name,age,department,experience);
+        Employee oldEmployee = new Employee(name, age, department, experience);
         String updateName = "John";
         int updateAge = 32;
         int updateExperience = 7;
 
         //when
         boolean isAdd = department.addEmployeeToDepartment(oldEmployee);
-        boolean isUpdate = department.updateEmployee(oldEmployee,new Employee(updateName,updateAge,department,updateExperience));
+        boolean isUpdate = department.updateEmployee(oldEmployee, new Employee(updateName, updateAge, department, updateExperience));
         //then
         assertTrue(isAdd);
         assertTrue(isUpdate);
 
     }
+
+
 }
