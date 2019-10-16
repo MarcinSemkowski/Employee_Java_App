5f478f1e6b21897e3a046ba913d768d797009cc6
Marcin <marcin96semkowski@gmail.com>
2019-10-16 17:09:22 +0200

    AddToFile #1

--- src/factory/menu_strategy/AddToFile.java
+++ src/factory/menu_strategy/AddToFile.java
@@ -8,9 +8,14 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.EmptyStackException;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class AddToFile implements CaseStrategy {
 
@@ -24,11 +29,40 @@ public class AddToFile implements CaseStrategy {
         Optional<Department> department = Optional.ofNullable(factory.getDepartmentMap().get(depName));
 
         if (department.isPresent()) {
+            System.out.println("Podaj ścieżkę do pliku, by zapisać.");
+            String fileName = scan.next();
 
+            Path path = Paths.get(fileName);
+
+            if (Files.exists(path)){
+                File file = new File(fileName);
+                addDepartmentToFile(file,department.get());
+
+            }else{
+                System.out.println("Nie znaleziono pliku stworzono nowy plik.");
+                File file = new File(fileName);
+                addDepartmentToFile(file,department.get());
+            }
         }
+    }
 
+    private boolean addDepartmentToFile(File file,Department department){
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+            bufferedWriter.write(department.getName());
+            bufferedWriter.newLine();
+            bufferedWriter.write(department.getEmployees()
+                    .stream()
+                    .map(Employee::toString)
+                    .collect(Collectors.joining(","))
+            );
+            return true;
+        }catch (IOException e){
+            return false;
+        }
     }
 
 
 }
+
+
 
