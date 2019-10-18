1b4f4d7defac2927d57c37e45297043d11dd72cf
Marcin <marcin96semkowski@gmail.com>
2019-10-18 12:00:33 +0200

    Create Factory in database and Department #1

--- src/factory/DB_Connection/CreateFactory.java
+++ src/factory/DB_Connection/CreateFactory.java
@@ -7,21 +7,31 @@ import java.sql.*;
 
 public class CreateFactory extends DB_Strategy {
 
-    String sqlFactory = "INSERT INTO factories(factory_name,factory_departments) " +
+
+
+
+    String sqlInsertFactory = "INSERT INTO factories(factory_name,factory_departments) " +
             "VALUES(?,?)";
+    String sqlInsertDepartment = "INSERT INTO Departments(dep_name,dep_employee) VALUES(?,?) ";
+
 
 
     @Override
-    protected boolean doCRUDTask(Factory factory, Department department) {
+    public boolean doCRUDTask(Factory factory, Department department) {
 
         try (Connection conn = DriverManager.getConnection(this.getUrl(), this.getUser(), this.getPassword());
         ) {
 
             conn.setAutoCommit(false);
 
-            try (PreparedStatement psInsert = conn.prepareStatement(sqlFactory)) {
-                psInsert.setString(1, factory.getName());
-                psInsert.setInt(2, 0);
+            try (PreparedStatement psInsertFac = conn.prepareStatement(sqlInsertFactory);
+                 PreparedStatement ps_InsertDep = conn.prepareStatement(sqlInsertDepartment)
+
+            ) {
+                ps_InsertDep.setString(1,department.getName());
+                ps_InsertDep.setInt(2,-1);
+                psInsertFac.setString(1, factory.getName());
+                psInsertFac.setInt(2, Department.getId());
                 conn.commit();
                 return true;
             } catch (SQLException e){
